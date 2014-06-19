7f2bbdb3d28059a2f80cd20963e7e1eae23b8de3
VIctor Bulnes <victor.bulnes@gmail.com>
2014-06-19 18:47:45 +0200

    Adding functions to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following the directrices provided by the example used in the Programming Assignment 2: Lexical Scoping,
+## I worte a pair of functions that returns the inverse of a given matrix, and cache the result for a
+## later use, avoiding redundant calculations.
 
-## Write a short comment describing this function
+##makeCacheMatrix function takes a matrix as an imput, and returns a list containing functions to:
+##1-.Set the value of the matrix
+##2-.Get the value of the vector
+##3-.Set the value of the inverted matrix
+##4-.Get the value of the inverted matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix()) {
+    ##initializing the variable that will store the inverted matrix (m)
+    m <- NULL
+    set <- function (y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverted) m <<- inverted
+    getinv <- function() m
+    list (set = set, get= get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##cachesolve function takes the output of makeCahceMatrix and checks whether or not inverted matrix (m)
+##have been already cached, otherwise it calculates and stores it before returning the result
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function (x,...) {
+    m <- x$getinv()
+    if (!is.null(m)) {
+        message("Returning cached inverted matrix")
+        return(m)
+    } else {
+        matrix <- x$get()
+        m <- solve(matrix)
+        x$setinv(m)
+    }
+    return(m)
 }
